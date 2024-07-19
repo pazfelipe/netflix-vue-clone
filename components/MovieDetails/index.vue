76599284b8d01bb1524d1796ea75ed74a978f6a0
Felipe Paz <xxfelipepaz@gmail.com>
2024-07-19 09:51:34 -0300

    refactor: impor movie genre component

--- components/MovieDetails/index.vue
+++ components/MovieDetails/index.vue
@@ -5,18 +5,9 @@
     </div>
     <div class="flex items-center text-lg pt-4">
       <div>{{ movie.year }}</div>
-      <div class="relative">
-        <span class="absolute left-[2px] -top-[12px] text-[40px]">.</span>
-        <span class="pl-4">{{ movie?.genre?.one }}</span>
-      </div>
-      <div class="relative">
-        <span class="absolute left-[2px] -top-[12px] text-[40px]">.</span>
-        <span class="pl-4">{{ movie?.genre?.two }}</span>
-      </div>
-      <div class="relative">
-        <span class="absolute left-[2px] -top-[12px] text-[40px]">.</span>
-        <span class="pl-4">{{ movie?.genre?.three }}</span>
-      </div>
+      <MovieDetailsGenre :genre="movie?.genre?.one" />
+      <MovieDetailsGenre :genre="movie?.genre?.two" />
+      <MovieDetailsGenre :genre="movie?.genre?.three" />
     </div>
   </div>
 </template>
