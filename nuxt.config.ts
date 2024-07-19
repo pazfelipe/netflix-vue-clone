ea3120e6178990a11fb4177ed8581fae0edc90ba
Felipe Paz <xxfelipepaz@gmail.com>
2024-07-19 12:16:18 -0300

    chore: add carousel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', "@nuxt/icon", "@nuxtjs/tailwindcss", "@nuxt/image"],
+  modules: [
+    '@pinia/nuxt',
+    "@nuxt/icon",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image",
+    "vue3-carousel-nuxt"
+  ],
   pinia: {
     storesDirs: ['./stores/**'],
   },
