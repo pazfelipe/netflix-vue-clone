7a1875d9fb46239ea2e57461719d8ad0245a3650
Felipe Paz <xxfelipepaz@gmail.com>
2024-07-19 09:07:27 -0300

    feature: create component MainMenu

---
+++ components/MainMenu/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="py-2 mx-10 my-6">
+      <Icon name="mdi:magnify" class="text-white text-5xl" />
+    </div>
+    <div class="py-2 mx-10 my-6 border-b-4 border-b-red-500">
+      <Icon name="mdi:home-outline" class="text-white text-5xl" />
+    </div>
+    <div class="py-2 mx-10 my-6">
+      <Icon name="mdi:trending-up" class="text-white text-5xl" />
+    </div>
+    <div class="py-2 mx-10 my-6">
+      <Icon name="mdi:television" class="text-white text-5xl" />
+    </div>
+    <div class="py-2 mx-10 my-6">
+      <Icon name="mdi:movie-outline" class="text-white text-5xl" />
+    </div>
+    <div class="py-2 mx-10 my-6">
+      <Icon name="mdi:plus" class="text-white text-5xl" />
+    </div>
+  </div>
+</template>
