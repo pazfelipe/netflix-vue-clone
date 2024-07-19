abb561b59a73c2047e40759bfcf7aa77e7eca70b
Felipe Paz <xxfelipepaz@gmail.com>
2024-07-19 09:51:25 -0300

    feature: create Movie Genre Component

---
+++ components/MovieDetails/Genre.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="relative">
+    <span class="absolute left-[2px] -top-[12px] text-[40px]">.</span>
+    <span class="pl-4">{{ props.genre }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+// @ts-ignore
+const props = defineProps({
+  genre: {
+    type: String
+  }
+})
+
+</script>
