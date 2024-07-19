ec45d7e61691e7b8bc18f287c85874f9b24b8260
Felipe Paz <xxfelipepaz@gmail.com>
2024-07-19 09:22:07 -0300

    feature: add netflix logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="fixe w-full h-screen bg-black">
     <div v-if="!showFullVideo" class="flex z-40 items-center w-[120px] h-screen bg-black relative">
-      N
+      <NuxtImg src="/images/netflix-logo.png" class="absolute top-0 w-[35px] mt-10 ml-10" />
       <MainMenu />
     </div>
   </div>
